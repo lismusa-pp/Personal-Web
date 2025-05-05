c68f94703f8e0a24e11710d5183215448db58167
lismusa-pp <lis.musa13@gmail.com>
2025-05-05 11:48:07 +0200

    Changed the style of buttons
    In the Kontakti section and also changed the design of  image in background  !

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
     console.log("Faqja u ngarkua me sukses!");
   });
 
-document.getElementById('darkModeToggle').addEventListener('click', () => {
+  document.getElementById('darkModeToggle').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-});
+  });
+  
   
