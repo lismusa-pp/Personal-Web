743757069a6b55926cfab1c7dba84be6c7b7b715
lismusa-pp <lis.musa13@gmail.com>
2025-05-06 00:41:30 +0200

    Changed Icons of Skills
    New design and behaviour in my website !

--- script.js
+++ script.js
@@ -6,4 +6,12 @@ document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.toggle('dark-mode');
   });
   
-  
+  // Fade-in on scroll
+$(window).on('scroll', function () {
+  $('section').each(function () {
+    const top = $(this).offset().top - 100;
+    if ($(window).scrollTop() > top) {
+      $(this).css('opacity', 1).css('transform', 'translateY(0)');
+    }
+  });
+});
